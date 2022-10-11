752751df6e70bba04faedeb117fb226e634c7b5c
ibnabii <bla@bla.pl>
2022-10-11 15:54:11 +0200

    Stage 3

--- Bike shop/task/shop/urls.py
+++ Bike shop/task/shop/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('bikes/', views.BikeListView.as_view(), name='bikeList')
+    path('bikes/', views.BikeListView.as_view(), name='bikeList'),
+    path('bikes/<int:bike_id>/', views.BikeDetailsView.as_view(), name='bikeDetails')
 ]
--- Bike shop/task/shop/views.py
+++ Bike shop/task/shop/views.py
@@ -1,10 +1,33 @@
 from django.views import View
+from django.views.generic.base import TemplateView
 from django.shortcuts import render
-from .models import Bike
+from django.http import Http404
+
+from . import models
 
 
 class BikeListView(View):
     def get(self, request, *args, **kwargs):
-        bikes = Bike.objects.all().values('name')
+        bikes = models.Bike.objects.all().values('name', 'id')
         context = {'bikes': bikes}
         return render(request, 'shop/bikeList.html', context=context)
+
+
+class BikeDetailsView(TemplateView):
+    template_name = 'shop/bikeDetails.html'
+
+    def get_context_data(self, **kwargs):
+        bike_id = kwargs.get('bike_id')
+        try:
+            bike = models.Bike.objects.get(id=bike_id)
+        except models.Bike.DoesNotExist:
+            raise Http404
+
+        if (bike.has_basket == True):
+            bike.has_basket = 'yes'
+        else:
+            bike.has_basket = 'no'
+
+        context = super().get_context_data(**kwargs)
+        context['bike'] = bike
+        return context
